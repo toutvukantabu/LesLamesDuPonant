e668d3a61135172c1c3c056cb0f34f0d54e6123a
Gwendal Bescont <64533628+toutvukantabu@users.noreply.github.com>
2020-09-02 16:59:54 +0200

    taf sur infos

--- LesLamesDuPonant/src/Controller/LesInfosController.php
+++ LesLamesDuPonant/src/Controller/LesInfosController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\HomeInfoRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LesInfosController extends AbstractController
 {
     /**
      * @Route("/les/infos", name="les_infos")
      */
-    public function index()
+    public function index(HomeInfoRepository $homeInfoRepository)
     {
         return $this->render('les_infos/index.html.twig', [
             'controller_name' => 'LesInfosController',
+            'home_infos' => $homeInfoRepository->findAll(),
         ]);
     }
 }
--- LesLamesDuPonant/src/Form/DisciplineType.php
+++ LesLamesDuPonant/src/Form/DisciplineType.php
@@ -33,10 +33,17 @@ class DisciplineType extends AbstractType
                '1'=>1,
                '2'=>2,
                '3'=>3,
+               '4'=>4,
+               '5'=>5,
+               '6'=>6,
+               '7'=>7,
+               '8'=>8,
+               '9'=>9,
             ],
             'multiple' => false,
             'expanded' => false,
             'required' => true,
+           	            
         ])
 
 
