cfed7fb9123204fa3b02e0924b5ceaa58c07f6d3
toutevukantabu <toutevukantabu@users.noreply.github.com>
2020-10-25 16:04:58 +0100

    25/10

--- LesLamesDuPonant/src/Security/AppAuthenticator.php
+++ LesLamesDuPonant/src/Security/AppAuthenticator.php
@@ -71,7 +71,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('l\'email est inconnu');
         }
 
         return $user;
